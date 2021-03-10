e80afda093707a802671374a435c24ab6e729e2b
wuergrob <wuergrob@students.zhaw.ch>
2021-03-10 10:07:10 +0100

    PokerCard PokerPlayerHandScore

---
+++ src/PokerCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public class PokerCard
+    {
+        public int rank;
+
+        public string suit;
+
+        public PokerCard(int rank, string suit)
+        {
+            this.rank = rank;
+            this.suit = suit;
+        }
+
+    }
+}
---
+++ src/PokerPlayerHandScore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    public class PokerPlayerHandScore
+    {
+        private PokerCard[] PlayerHand = new PokerCard[2];
+
+        private PokerCard[] BoardCards = new PokerCard[3];
+
+        public PokerPlayerHandScore(PokerCard[] cards)
+        {
+            PlayerHand = cards;
+        }
+
+
+
+    }
+}
