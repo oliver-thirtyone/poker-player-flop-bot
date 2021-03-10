28a0c129d15c26bb50f45818ddeba9d581fd50b1
Oliver S <r3zn1k@bluewin.ch>
2021-03-10 09:43:43 +0100

    Always bet 200

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -8,8 +8,7 @@ namespace Nancy.Simple
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 0;
+			return 200;
 		}
 
 		public static void ShowDown(JObject gameState)
